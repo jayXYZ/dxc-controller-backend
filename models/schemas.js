8397798a0d69be2743d88a2271e4c945091f32ff
Jay McCowan <jaymccowan@Jays-MacBook-Pro-3.local>
2024-08-14 13:48:19 -0400

    first commit :)

---
+++ models/schemas.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const dataSchema = new Schema({
+    id: {type:String, required:true},
+    p1name: {type:String, default:''},
+    p1deck: {type:String, default:''},
+    p1life: {type:Number, default:20},
+    p1record: {type:String, default:''},
+    p1gameswon: {type:Number, default:0},
+    p2name: {type:String, default:''},
+    p2deck: {type:String, default:''},
+    p2life: {type:Number, default:20},
+    p2deck: {type:String, default:''},
+    p2record: {type:String, default:''},
+    p2gameswon: {type:Number, default:0},
+    event: {type:String, default:''},
+    format: {type:String, default:'Premodern'},
+    commentators: {type:String, default:''},
+    round: {type:String, default:''},
+    cardimage: {type:String, default:'https://cards.scryfall.io/png/front/c/a/ca367f49-0f4a-4b7f-8104-851893fbcd8a.png?1562937711'}
+})
+
+const Data = mongoose.model('Data', dataSchema, 'data')
+
+export default Data
---
+++ src/server.js
@@ -0,0 +1,69 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import Data from '../models/schemas.js'
+
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,
+//   optionSuccessStatus: 200
+// }
+
+mongoose.connect(process.env.DB_URI)
+.then(console.log('DB connected!'))
+.catch(err => console.log(err))
+
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    methods: ['GET', 'POST'],
+  },
+});
+
+
+io.on('connection', async(socket) => {
+  console.log('a user connected');
+
+  socket.on('check_if_active', async(instancename, callback) => {
+    const query = await Data.find({ id: instancename }).exec()
+    callback(query);
+  })
+
+  socket.on('create_instance', async(instancename) => {
+    await new Data({id:instancename}).save();
+  })
+
+  socket.on('join_instance', async(instancename, callback) => {
+    const currentData = await Data.findOne({id: instancename})
+    socket.join(instancename);
+    callback(currentData);
+    console.log('added user to room ' + instancename);
+  })
+
+  socket.on('update_data', async(instancename, data) => {
+    const updatedData = await Data.findOneAndUpdate({id: instancename}, data, {new: true});
+    socket.to(instancename).emit('server_update', updatedData);
+  })
+
+  socket.on('disconnect', () => {
+    // on dc check to see how many remaining connections 
+    // wait this doesn't work how i want it to
+    // need to check how many sockets are in a room?
+    // so i need to create an array that stores all the connections
+    // that get added to a room?
+
+  })
+});
+
+
+server.listen(port, ()=> {
+  console.log(`Server is running on port ${port}`)
+});
