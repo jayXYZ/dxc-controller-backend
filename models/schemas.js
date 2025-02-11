29f8032e49874355d9ac01de62081c82d1bebf61
Jay McCowan <jaymccowan13@gmail.com>
2025-02-11 14:44:32 -0500

    added decklists and deckviewstate to schema

--- models/schemas.js
+++ models/schemas.js
@@ -6,6 +6,7 @@ const dataSchema = new Schema({
     id: {type:String, required:true},
     p1name: {type:String, default:''},
     p1deck: {type:String, default:''},
+    p1decklist: {type:Array, default:[]},
     p1life: {type:Number, default:20},
     p1record: {type:String, default:''},
     p1gameswon: {type:Number, default:0},
@@ -13,6 +14,7 @@ const dataSchema = new Schema({
     p2deck: {type:String, default:''},
     p2life: {type:Number, default:20},
     p2deck: {type:String, default:''},
+    p2decklist: {type:Array, default:[]},
     p2record: {type:String, default:''},
     p2gameswon: {type:Number, default:0},
     event: {type:String, default:''},
@@ -21,7 +23,8 @@ const dataSchema = new Schema({
     round: {type:String, default:''},
     cardimage: {type:String, default:'https://cards.scryfall.io/png/front/c/a/ca367f49-0f4a-4b7f-8104-851893fbcd8a.png?1562937711'},
     timerExpiry: {type:Date, default:null},
-    timerIsRunning: {type:Boolean, default:false}
+    timerIsRunning: {type:Boolean, default:false},
+    deckviewstate: {type:String, default:null}
 })
 
 const Data = mongoose.model('Data', dataSchema, 'data')
